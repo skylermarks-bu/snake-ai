1146c593821602e8e9008db14b49f0dd23a89e68
Skyler Marks <sky@localhost.my.domain>
2023-05-17 21:25:55 -0400

    Finish basics     - Implement torus map     - Implement optimized tracking     - Clean up files     - Implement energy with a nice little bar
    TODO:
        - Drop food on mouse click
        - Maybe larger map
        - Use neural network to find food instead of handwritten algorythm

--- ai.py
+++ ai.py
@@ -2,19 +2,39 @@ import numpy as np
 print("AI imported")
 
 def action(me, target):
-	dif  = me - target
+	dif  = torusvecmin(me, target, 500)
 	out = (float(abs(dif[0]) > abs(dif[1]))*sign(dif[0]), float(abs(dif[0]) <= abs(dif[1]))*sign(dif[1]))
 	"""
 			The above is some dark magic. Python casts booleans to one and zero,
 			so I've set  the first component of out to one if the first
 			component is larger and the second to one if the second component is
 			larger. I then multiply each component of out by the sign of the
-			component of dif. This means that if 
+			component of dif. This means that if one or both of the components
+			are going backwards, the component will continue to point in the
+			same direction after we've messed with it. This leaves a unit vector
+			in either the positive or negative of the basis vectors, which is
+			what we need to move in one of the four directions as the snake. 
 	"""
-	print(out)
 	return out
 
 def sign(x):
 	if x == 0:
 		return x
 	return x/abs(x)
+
+def torusvecmin(a, b, loop):
+	lengths  = []
+	vectors  = []
+	print(a)
+	print(b)
+	for i in [-500, 0, 500]:
+		for j in (-500, 0, 500):
+			vec = np.array((i, j))
+			vector = a-b-vec
+			vectors.append(vector)
+			lengths.append(abs(np.linalg.norm(vector)))
+	print(vectors)
+	ret = (vectors[lengths.index(min(lengths))])
+	return ret
+
+
--- snake.py
+++ snake.py
@@ -15,28 +15,43 @@ dims = 30		# Set the width and height of the squares
 
 def run_game():	# This function will run a single round of the game
 	global dims
-	energy = 10
+	energy = 11
 	count = 0
-	agentpos = np.array((100, 100))	#The position of the worm is a vector in R2, or a touple
+	agentpos = np.array((400, 100))	#The position of the worm is a vector in R2, or a touple
 	applepos = np.array((400, 400))	#with two values
 	action = ai.action(agentpos, applepos) 
 	while True:
 		time.sleep(0.01)
+
+		if agentpos[0] > 500:	# Wrap on a torus
+			agentpos[0] = 0		# This is quite crude at the moment; eventually
+		if agentpos[1] > 500:	# I'll write some code to make the rectangle 
+			agentpos[1] = 0		# go off one side of the screen and come back on
+								# the other smoothly instead of just jumping.
+		if agentpos[0] < 0:	
+			agentpos[0] = 500		
+		if agentpos[1] < 0:	
+			agentpos[1] = 500		
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return False
 	
 		if count < dims:
+			energy -= 1/dims
 			agentpos = agentpos - action
 			count += 1
 		else:
 			action = ai.action(agentpos, applepos) 
 			dir = np.array((float(action[0] < action[1]), float(action[1] >= action[0])))
 			count = 0
-		agent_rect = pygame.Rect(agentpos[0]+dims/2, agentpos[1]-dims/2, dims, dims) # Rectangle for the agent
+		agent_rect = pygame.Rect(agentpos[0]-dims/2, agentpos[1]-dims/2, dims, dims) # Rectangle for the agent
 		apple_rect = pygame.Rect(applepos[0]-dims/2, applepos[1]-dims/2, dims, dims) # Rectangle for the apple
+		energy_bar = pygame.Rect(10, 10, energy * 10, 10)
+		
 
 		if agent_rect.colliderect(apple_rect):
+			energy += 15
 			return True
 		if energy <= 0:
 			return False
@@ -46,6 +61,8 @@ def run_game():	# This function will run a single round of the game
 	
 		pygame.draw.rect(screen, (0, 255, 0),	# Draw a square where the worm is
 			agent_rect)
+		pygame.draw.rect(screen, (0, 255, 0),	# Draw a square where the worm is
+			energy_bar)
 		pygame.draw.rect(screen, (255, 0, 0),	# Draw a square where the apple is
 			apple_rect)
 	
